e1caf05cd1a9f31cd1cb06cc8a38ae2a0835f4aa
antony-jr <antonyjr@protonmail.com>
2023-11-06 22:52:17 +0530

    Backport fixes.

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -19,5 +19,5 @@ fi
 
 cd /ham-output
 
-export TODAY=$(date +"%Y%m%d")
+export TODAY=$(date +"%Y%m%d%H%M%S")
 ghr -delete -t "${GITHUB_TOKEN}" -u "${GITHUB_USER}" -r "${GITHUB_REPO}" "${TODAY}" /ham-output/
